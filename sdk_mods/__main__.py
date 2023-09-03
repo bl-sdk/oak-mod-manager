050089eb39db109f758511289754ddca839c5735
apple1417 <git@apple1417.dev>
2023-09-03 16:15:45 +1200

    better failed import logging

--- sdk_mods/__main__.py
+++ sdk_mods/__main__.py
@@ -2,12 +2,12 @@ import importlib
 import traceback
 from pathlib import Path
 
-import unrealsdk
+from unrealsdk import logging
 
 _full_traceback = False
 
 
-while not unrealsdk.logging.is_console_ready():
+while not logging.is_console_ready():
     pass
 
 for folder in Path(__file__).parent.iterdir():
@@ -17,12 +17,20 @@ for folder in Path(__file__).parent.iterdir():
     if folder.name.startswith(".") or folder.name == "__pycache__":
         continue
 
+    if not (folder / "__init__.py").exists() and (folder / folder.name / "__init__.py").exists():
+        logging.error(
+            f"'{folder.name}' appears to be double nested, which may prevent it from being it from"
+            f" being loaded. Move the inner folder up a level.",
+        )
+
     try:
         importlib.import_module(folder.name)
     except Exception as ex:  # noqa: BLE001
-        print(f"Failed to import mod: {folder.name}")
+        logging.error(f"Failed to import mod '{folder.name}'")
 
         tb = traceback.extract_tb(ex.__traceback__)
         if not _full_traceback:
             tb = tb[-1:]
-        print("".join(traceback.format_list(tb)))
+
+        logging.error("".join(traceback.format_exception_only(ex)))
+        logging.error("".join(traceback.format_list(tb)))
