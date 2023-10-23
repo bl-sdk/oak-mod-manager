87f9e7844b02365f6eb8e7dc567e1ce12df06c07
apple1417 <git@apple1417.dev>
2023-10-23 13:06:03 +1300

    detect and given warnings for more common installation errors

--- src/__main__.py
+++ src/__main__.py
@@ -19,17 +19,51 @@ for entry in Path(__file__).parent.iterdir():
         if entry.name.startswith(".") or entry.name == "__pycache__":
             continue
 
+        # A lot of people accidentally extract into double nested folders - they have a
+        # `sdk_mods/MyCoolMod/MyCoolMod/__init__.py` instead of a `sdk_mods/MyCoolMod/__init__.py`
+        # Usually this silently fails - we import `MyCoolMod` but there's nothing there
+        # Detect this and give a proper error message
         if not (entry / "__init__.py").exists() and (entry / entry.name / "__init__.py").exists():
             logging.error(
                 f"'{entry.name}' appears to be double nested, which may prevent it from being it"
                 f" from being loaded. Move the inner folder up a level.",
             )
+            # Since it's a silent error, may as well continue in case it's actually what you wanted
+
+        # In the case we have a `sdk_mods/My Cool Mod v1.2`, python will try import `My Cool Mod v1`
+        # first, and fail when it doesn't exist. Try detect this to throw a better error.
+        # When this happens we're likely also double nested - `sdk_mods/My Cool Mod v1.2/MyCoolMod`
+        # - but we can't detect that as easily, and the problem's the same anyway
+        if "." in entry.name:
+            logging.error(
+                f"'{entry.name}' is not a valid python module - have you extracted the right"
+                f" folder?",
+            )
+            continue
 
         mods_to_import.append(entry.name)
 
     elif entry.is_file():
-        if entry.name.startswith(".") or entry.suffix != ".sdkmod":
+        if entry.name.startswith("."):
             continue
+
+        match entry.suffix.lower():
+            # Since hotfix mods can be any text file, this won't be exhaustive, but match and warn
+            # about what we can
+            # OHL often uses .url files to download the latest version of a mod, so also match that
+            case ".bl3hotfix" | ".wlhotfix" | ".url":
+                logging.error(
+                    f"'{entry.name}' appears to be a hotfix mod, not an SDK mod. Move it to your"
+                    f" hotfix mods folder.",
+                )
+                continue
+
+            case ".sdkmod":
+                # Handled below the match
+                pass
+
+            case _:
+                continue
 
         valid_zip: bool
         try:
